0a18b9f4c05e99b48ed64620cbac00db98e82d5b
shet1990 <shet1990@gmail.com>
2018-10-05 00:12:01 +0300

    smart-calculator

--- src/index.js
+++ src/index.js
@@ -1,27 +1,32 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
+    constructor(initialValue) {
+        this.result = initialValue;
+        this.toString = () => eval(this.result);
+    }
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
+    add(number) {
+        this.result = this.result + '+' + number;
+        return this;
+    }
 
-  multiply(number) {
-    // your implementation
-  }
+    subtract(number) {
+        this.result = this.result + '-' + number;
+        return this;
+    }
 
-  devide(number) {
-    // your implementation
-  }
+    multiply(number) {
+        this.result = this.result + '*' + number;
+        return this;
+    }
 
-  pow(number) {
-    // your implementation
-  }
+    devide(number) {
+        this.result = this.result + '/' + number;
+        return this;
+    }
+
+    pow(number) {
+        this.result = this.result + '**' + number;
+        return this;
+    }
 }
-
 module.exports = SmartCalculator;
